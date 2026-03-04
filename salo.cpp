dc7046f4896ef7133aed67ecf999a21c6cf42e73
leukdn <daft1197@icloud.com>
2026-03-04 18:51:42 +0300

    My first comment

---
+++ salo.cpp
@@ -0,0 +1,39 @@
+#include "newtest.h"
+#include <QtTest/QtTest>
+#include <QTest>
+#include "newclass.h"
+
+newtest::newtest(QObject *parent)
+    : QObject{parent}
+{}
+
+void newtest::testsynonm_data()
+{
+    QTest::addColumn<QList<QStringList>>("Dictionary");
+    QTest::addColumn<QString>("word");
+    QTest::addColumn<QString>("synonym");
+
+    QTest::newRow("test1")<<QList<QStringList>{{"big", "large", "huge"}}<<"big"<<"large";
+
+    QTest::newRow("test2")<<QList<QStringList>{{"big", "large", "huge"}}<<"large"<<"big";
+
+    QTest::newRow("test3")<<QList<QStringList>{{"big", "large", "huge"},{"small","little"}}<<"small"<<"little";
+
+    QTest::newRow("test4")<<QList<QStringList>{{"big", "large", "huge"}}<<"nan"<<"";
+
+}
+void newtest::testsynonm()
+{
+    QFETCH(QList<QStringList>,Dictionary);
+    QFETCH(QString, word);
+    QFETCH(QString, synonym);
+
+    QString result;
+
+    findSynonym(Dictionary, word, result);
+
+    QCOMPARE(result, synonym);
+
+
+
+}
